301029718ee8609266a3a92896185310ebdc4d7e
Bill Mill <bill@billmill.org>
2023-11-28 22:06:15 -0500

    from_into

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s == "" {
+            return Person::default();
+        }
+        let parts: Vec<_> = s.split(",").collect();
+        print!("parts {:?}", parts);
+        if parts.len() < 2 || parts[0] == "" {
+            return Person::default();
+        }
+
+        let age = parts[1].parse::<usize>();
+        match age {
+            Ok(age) => {
+                return Person {
+                    name: parts[0].into(),
+                    age,
+                }
+            }
+            Err(age) => Person::default(),
+        }
     }
 }
 
